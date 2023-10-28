c310291487117b7ada7af6bd25e9c605a04a312d
erichstuder <richu_@hotmail.com>
2023-10-28 15:12:13 +0200

    add and use of local headers

---
+++ local_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p temp
+cd temp
+
+# get cucumber-cpp headers
+git clone https://github.com/cucumber/cucumber-cpp.git
+cd cucumber-cpp
+git submodule init
+git submodule update
+# execute necessary build steps to create missing include file(s)
+cmake -E make_directory build
+cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCMAKE_INSTALL_PREFIX=${prefix} ..
+cd ..
+
+# get gtest headers
+git clone https://github.com/google/googletest.git

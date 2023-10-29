c3c789cb55b79ecfd55f75c3d34c92ec941661a0
erichstuder <richu_@hotmail.com>
2023-10-29 05:05:22 +0100

    implement tests

---
+++ IO-Link Interface and System/9 System Management/9.3 System Management of the Device/9.3.2 SM Device services/9.3.2.2 SM_SetDeviceCom/9.3.2.2 SM_SetDeviceCom.cpp
@@ -0,0 +1,6 @@
+#include "9.3.2.2 SM_SetDeviceCom.h"
+
+Result_type SM_SetDeviceCom(SM_SetDeviceCom__parameters parameters) {
+	(void) parameters; //prevent unused paramter warning for now
+	return Result_type::PARAMETER_CONFLICT;
+}
--- IO-Link Interface and System/9 System Management/9.3 System Management of the Device/9.3.2 SM Device services/9.3.2.2 SM_SetDeviceCom/features/step_definitions/9.3.2.2 SM_SetDeviceCom__steps.cpp
+++ IO-Link Interface and System/9 System Management/9.3 System Management of the Device/9.3.2 SM Device services/9.3.2.2 SM_SetDeviceCom/features/step_definitions/9.3.2.2 SM_SetDeviceCom__steps.cpp
@@ -3,80 +3,60 @@
 
 #include "9.3.2.2 SM_SetDeviceCom.h"
 
-/*#include <regex>
-#include <boost/range/irange.hpp>
-
-using std::string; //TODO: braucht es das? Wie funktioniert das genau?
-
-using namespace A_2_M_sequence_types;
-
-static struct {
-	uint8_t OPERATE_M_sequence_code_min;
-	uint8_t OPERATE_M_sequence_code_max;
-	uint8_t On_request_Data_Octets;
-	uint8_t PDin_min;
-	uint8_t PDin_max;
-	uint8_t PDout_min;
-	uint8_t PDout_max;
-	PD_representation_type PD_representation;
-	M_sequence_type expected_M_sequence_type;
-} context;
-
-enum class Table_ID {
-	A9,
-	A10
-};*/
-
 
 GIVEN("^SupportedSIOMode is in range \\{INACTIVE, DI, DO\\}$") {
-	//check whether the values exist
-	EXPECT_TRUE((int)SupportedSIOMode::INACTIVE >= 0);
-	EXPECT_TRUE((int)SupportedSIOMode::DI >= 0);
-	EXPECT_TRUE((int)SupportedSIOMode::DO >= 0);
-	EXPECT_EQ((int)SupportedSIOMode::_cnt, 3);
+	EXPECT_TRUE((int)SupportedSIOMode_type::INACTIVE >= 0);
+	EXPECT_TRUE((int)SupportedSIOMode_type::DI >= 0);
+	EXPECT_TRUE((int)SupportedSIOMode_type::DO >= 0);
+	EXPECT_EQ((int)SupportedSIOMode_type::_cnt, 3);
 }
 
 GIVEN("^SupportedTransmissionrate is in range \\{COM1, COM2, COM3\\}$") {
-	//check whether the values exist
-	EXPECT_TRUE((int)SupportedTransmissionrate::COM1 >= 0);
-	EXPECT_TRUE((int)SupportedTransmissionrate::COM2 >= 0);
-	EXPECT_TRUE((int)SupportedTransmissionrate::COM3 >= 0);
-	EXPECT_EQ((int)SupportedTransmissionrate::_cnt, 3);
+	EXPECT_TRUE((int)SupportedTransmissionrate_type::COM1 >= 0);
+	EXPECT_TRUE((int)SupportedTransmissionrate_type::COM2 >= 0);
+	EXPECT_TRUE((int)SupportedTransmissionrate_type::COM3 >= 0);
+	EXPECT_EQ((int)SupportedTransmissionrate_type::_cnt, 3);
 }
 
 GIVEN("^MinCycleTime has any value$") {
-	//TODO: check the type?
-	FAIL();
+	SUCCEED();
 }
 
 GIVEN("^M-sequence Capability is in range \\{ISDU, OPERATE, PREOPERATE\\}$") {
-	//check whether the values exist
-	EXPECT_TRUE((int)M_sequence_Capability::ISDU >= 0);
-	EXPECT_TRUE((int)M_sequence_Capability::OPERATE >= 0);
-	EXPECT_TRUE((int)M_sequence_Capability::PREOPERATE >= 0);
-	EXPECT_EQ((int)M_sequence_Capability::_cnt, 3);
+	EXPECT_TRUE((int)M_sequence_Capability_type::ISDU >= 0);
+	EXPECT_TRUE((int)M_sequence_Capability_type::OPERATE >= 0);
+	EXPECT_TRUE((int)M_sequence_Capability_type::PREOPERATE >= 0);
+	EXPECT_EQ((int)M_sequence_Capability_type::_cnt, 3);
 }
 
 GIVEN("^RevisionID \\(RID\\) has any value$") {
-	//TODO: check the type?
-	FAIL();
+	SUCCEED();
 }
 
 GIVEN("^ProcessDataIn has any value$") {
-	//TODO: check the type?
-	FAIL();
+	SUCCEED();
 }
 
 GIVEN("^ProcessDataOut has any value$") {
-	//TODO: check the type?
-	FAIL();
+	SUCCEED();
 }
 
 THEN("^the Arguments are in passable range$") {
-	//TODO: check if the service takes the above types
-	FAIL();
+	// check whether the service takes the above types
+	SM_SetDeviceCom__parameters parameters;
+	parameters.SupportedSIOMode = SupportedSIOMode_type::INACTIVE;
+	parameters.SupportedTransmissionrate = SupportedTransmissionrate_type::COM1;
+	parameters.MinCycleTime = 1.1f;
+	parameters.M_sequence_Capability = M_sequence_Capability_type::ISDU;
+	parameters.RevisionID = 42;
+	parameters.ProcessDataIn = 100;
+	parameters.ProcessDataOut = 72;
+
+	SM_SetDeviceCom(parameters);
 }
 
-/*THEN("^the Result can only have the values SUCCESS and PARAMETER_CONFLICT$") {
-	FAIL();
-}*/
+THEN("^the Result can only have the values SUCCESS and PARAMETER_CONFLICT$") {
+	EXPECT_TRUE((int)Result_type::SUCCESS >= 0);
+	EXPECT_TRUE((int)Result_type::PARAMETER_CONFLICT >= 0);
+	EXPECT_EQ((int)Result_type::_cnt, 2);
+}

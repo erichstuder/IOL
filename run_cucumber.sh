bcfa560424f5254e3166e8c2ae78a5ef3b81d89f
erichstuder <richu_@hotmail.com>
2023-11-08 19:58:33 +0100

    move build to tmp folder

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -2,13 +2,16 @@
 
 # make sure the tag is unique
 TAG=$(echo ${PWD:1} | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]')
-WORKDIR=/usr/software
+VOLUME_DIR=/usr/software
 echo "building container, please wait (the first time this may take several minutes) ..."
 docker build -t $TAG . >/dev/nul
+
+BUILD_DIR=/tmp/cucumber_build
+mkdir -p ${BUILD_DIR}
 docker run \
 	--rm \
 	--name cucumber_$(date +%s) \
-	--volume "${PWD}":${WORKDIR} \
-	--workdir "${WORKDIR}" \
+	--volume "${PWD}":${VOLUME_DIR} \
+	--workdir "${VOLUME_DIR}${BUILD_DIR}" \
 	-it $TAG \
-	bash -c "cmake . && cmake --build . && (./steps >/dev/null &) && cucumber ."
+	bash -c "cmake ${VOLUME_DIR} && cmake --build . && (./steps >/dev/null &) && cucumber ${VOLUME_DIR}"
